76f060b4dc45ed6951c082ce0149748ebe197b71
Ben Gunn <bgunn@bens-mbp.bigpond>
2015-05-24 16:22:55 +1000

    Create template file and modify page preprocess function used for comment rely page to display node title as part of page title.

--- dto_whitesite/template.php
+++ dto_whitesite/template.php
@@ -53,11 +53,15 @@ function dto_whitesite_preprocess_html(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("page" in this case.)
  */
-/* -- Delete this line if you want to use this function
+
 function dto_whitesite_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
+  
+  // Get node title if is comment reply page. Used in page--comment-reply.tpl.php
+  if (!empty($vars['page']['content']['system_main']['comment_node']['#node'])) {
+    $node = $vars['page']['content']['system_main']['comment_node']['#node'];
+    $vars['node_title'] = check_plain($node->title);
+  }
 }
-// */
 
 /**
  * Override or insert variables into the node templates.
