98dbd331f41d5196aaa457ab41743db66668f1fc
Ben Gunn <ben@technocrat.com.au>
2015-05-25 11:48:07 +1000

    Add alt tags for site logo and logo link to improve accessibility.

--- dto_whitesite/templates/page.tpl.php
+++ dto_whitesite/templates/page.tpl.php
@@ -10,7 +10,7 @@
 
 <div class="layout-centered page-wrapper">
   <header class="header" role="banner">
-    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+    <a href="<?php print $front_page; ?>" title="<?php print t('Link to Digital Transformation Office home page'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Digital Translformation Office'); ?>" class="header__logo-image" /></a>
 
     <nav class="header__secondary-menu" role="navigation">
       <?php print theme('links__system_secondary_menu', array(
