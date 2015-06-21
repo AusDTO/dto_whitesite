6814ea0133460656f3479120f71ec945cfc84f75
simonschwartz <localadmin@webmacbz.local>
2015-06-21 17:53:18 +1000

    IE8 fixes

--- dto_whitesite/templates/page.tpl.php
+++ dto_whitesite/templates/page.tpl.php
@@ -10,6 +10,7 @@
 
 <!--<div class="layout-centered page-wrapper">-->
 <div class="menu-open"></div>
+<div class="ie-warning"><span class="warning-text">It looks like you're using an old version of Internet Explorer. For the best experience, please update your browser.</span></div>
   <header class="header" role="banner">
   <div class="nav-head-container">
     <!--<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /><div class="mobile-header">Digital Transformation Office</div></a>-->
