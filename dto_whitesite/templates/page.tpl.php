65df3cd30af994f5d3887fcd05a47080daf48397
DTO Mac 2 <dtomac2@DTOs-MacBook-Pro.local>
2015-07-28 17:23:33 +1000

    added link to DTO github in footer

--- dto_whitesite/templates/page.tpl.php
+++ dto_whitesite/templates/page.tpl.php
@@ -115,6 +115,7 @@
     <a href="https://twitter.com/AusDTO" title="DTO Twitter"><div class="twitter-icon"></div></a>
     <a href="https://www.youtube.com/channel/UCmDkFN3UlK2wSKDQQhd-Y-A" title="DTO Youtube"><div class="youtube-icon"></div></a>
     <a href="https://www.linkedin.com/company/digital-transformation-office" title="DTO Linkedin"><div class="linkedin-icon"></div></a>
+    <a href="https://github.com/AusDTO" title="DTO Github"><div class="github-icon"></div></a>
     </div>
     <div class="footer-links">
    <!--<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="sites/all/themes/custom/dto_whitesite/images/update/government-crest.png" alt="<?php print t('Home'); ?>" class="header__logo-image" /><span class="title">Digital Transformation Office</span></a>-->
