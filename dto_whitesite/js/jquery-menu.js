9486244d6106f36d59ec60e795efa48af72f9c79
DTO Mac 2 <dtomac2@DTOs-MacBook-Pro.local>
2015-07-28 13:10:59 +1000

    added js to make twitter block exceed 520 width

--- dto_whitesite/js/jquery-menu.js
+++ dto_whitesite/js/jquery-menu.js
@@ -6,3 +6,13 @@ jQuery(document).ready(function() {
         jQuery('body').removeClass('show');
    	});
 });
+
+
+twttr.events.bind('rendered',function(){
+  [].slice.call(document.querySelectorAll('iframe.twitter-timeline')).forEach(function(e,i,a){
+    var fE = e.contentDocument.getElementsByClassName('timeline');
+    if(fE.length){
+      fE[0].style.maxWidth='720px';
+    }
+  });
+});
