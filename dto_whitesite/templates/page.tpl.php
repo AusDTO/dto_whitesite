556a58cb977c6020194ae2c4002f4624fa3efe5e
Simon Schwartz <dtomac2@DTOs-MacBook-Pro.local>
2015-08-25 20:40:17 +1000

    Added embedded recruitment form and styling

--- dto_whitesite/templates/page.tpl.php
+++ dto_whitesite/templates/page.tpl.php
@@ -105,11 +105,36 @@
 
   </div>
 
-  
+<div class="recruitment-form">
+	<script type="text/javascript" id="rbox-loader-script">
+if(!window._rbox){
+_rbox = { host_protocol:document.location.protocol, ready:function(cb){this.onready=cb;} };
+(function(d, e) {
+    var s, t, i, src=['/static/client-src-served/widget/36555/rbox_api.js', '/static/client-src-served/widget/36555/rbox_impl.js'];
+    t = d.getElementsByTagName(e); t=t[t.length - 1];
+    for(i=0; i<src.length; i++) {
+        s = d.createElement(e); s.src = _rbox.host_protocol + '//w.recruiterbox.com' + eval("src" + String.fromCharCode(91) + String(i) + String.fromCharCode(93));
+        t.parentNode.insertBefore(s, t.nextSibling);
+    }})(document, 'script');
+}
+</script>
+</div>	  
 
 </div>
 
 <?php print render($page['footer_top']); ?>
+<script type="text/javascript" id="rbox-loader-script">
+if(!window._rbox){
+_rbox = { host_protocol:document.location.protocol, ready:function(cb){this.onready=cb;} };
+(function(d, e) {
+    var s, t, i, src=['/static/client-src-served/widget/36555/rbox_api.js', '/static/client-src-served/widget/36555/rbox_impl.js'];
+    t = d.getElementsByTagName(e); t=t[t.length - 1];
+    for(i=0; i<src.length; i++) {
+        s = d.createElement(e); s.src = _rbox.host_protocol + '//w.recruiterbox.com' + eval("src" + String.fromCharCode(91) + String(i) + String.fromCharCode(93));
+        t.parentNode.insertBefore(s, t.nextSibling);
+    }})(document, 'script');
+}
+</script>
 <footer class="footer">
 	<div class="connect-with-dto">
     <h2>Connect with DTO</h2>
