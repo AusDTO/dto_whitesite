a3e51b1425e38a6baacd7aa3df8b99e411efff53
Simon Schwartz <dtomac2@DTOs-MacBook-Pro.local>
2015-10-07 18:09:59 +1100

    Actioned fixes from initial testing

--- dto_whitesite/js/jquery-menu.js
+++ dto_whitesite/js/jquery-menu.js
@@ -2,7 +2,7 @@ if (jQuery(window).width() > 580) {
 	window.ATL_JQ_PAGE_PROPS =  {
 		"triggerFunction": function(showCollectorDialog) {
 		//Requries that jQuery is available! 
-			jQuery("#feedbackTrigger").click(function(e) {
+			jQuery(".feedbackTrigger").click(function(e) {
 				e.preventDefault();
 				showCollectorDialog();
 			});
